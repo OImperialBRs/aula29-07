b114e05bf0cbe63038d7507c2fcbe238ea585140
pedroH Araujo <pedroha2001@gmail.com>
2024-07-31 09:14:18 -0400

    add mais um usuario

--- sistema.ts
+++ sistema.ts
@@ -22,3 +22,25 @@ carro1.set_cor("preto");
 carro1.set_modelo("HB20");
 carro1.set_nivelDeUso(100000);
 console.log(carro1);
+
+
+let pessoa2 = new Pessoa("", 0o6216602136, new Date(""));
+pessoa2.set_nome("PEDRO");
+pessoa2.set_cpf(00200100365);
+pessoa2.set_dataNascimento(new Date ("200-08-04"));
+
+let documento2 = new Documento(new Date(""), "", pessoa1, "", 123456, new Date(""), "");
+documento2.set_ano(new Date ("2023-09-18"));
+documento2.set_placa("FAKER789");
+documento2.set_exProprietario(pessoa2);
+documento2.set_carroceria("sedam");
+documento2.set_renavam(654321);
+documento2.set_dataLicenciamento(new Date("2023-10-01"));
+documento2.set_statusDocumento(new Date("2024-10-01"));
+
+let carro2 = new Veiculo(documento2, " ", " ", 50000, " ");
+carro2.set_documento(documento2);
+carro2.set_cor("laranja");
+carro2.set_modelo("mclata");
+carro2.set_nivelDeUso(100000);
+console.log(carro2);
